c1976af5445940915e19d5f438ae8990664cab8f
wtam <william.tam.2013@hotmail.com.hk>
2016-11-15 10:47:25 +0800

    correct dummy timmer AzFn codes

--- dummy_timer/index.js
+++ dummy_timer/index.js
@@ -2,9 +2,9 @@ module.exports = function (context, myTimer) {
     context.log("Timer triggered at " + myTimer.next);
     var https = require('https'); 
     var pingPaths = [
-        '/api/devices?action=ping&code=i/KJ0BOPCiiSFfamZ3YLRxEtajzFdgtUkZ7nwe2pc6RpireVRTXvuw==',
-        '/api/messages?action=ping&code=TQXpK4bqC11EPbHeY6sWzbR8Hg318TyR1VnukNlV1XaLvHbHfx1MYQ==',
-        '/api/message_feed?action=ping&code=a3G5z/Dt2yWvQL0VCyzaeQrnW1VrkbE5lH6kbsNuYOeDhsf5A4LUrw=='
+        'api/devices?action=ping&code=S2BDCKLb1irqPU/iK5ijW1LvflNan8Drav9azvpC4ISa6A0oNeXp0A==',
+        '/api/messages?action=ping&code=blfUL7SXmgc4gV1qQM9MFNIFymUh3AJEY7wDiMa3fxWyslmCEVvGpg==',
+        '/api/message_feed?action=ping&code=nGYiKKC/z3FLR3yZb3sY71259NOtSVhqv/c178Ta5BJKsQTHe181AQ=='
     ]
 
     context.log("timer passed?",myTimer.isPastDue)
