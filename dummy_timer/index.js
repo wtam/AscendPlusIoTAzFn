d410016c2ede9235fdd9c2c3b53adbbaac105d9c
wtam <william.tam.2013@hotmail.com.hk>
2016-11-16 10:06:05 +0800

    add missing / into ping path

--- dummy_timer/index.js
+++ dummy_timer/index.js
@@ -2,7 +2,7 @@ module.exports = function (context, myTimer) {
     context.log("Timer triggered at " + myTimer.next);
     var https = require('https'); 
     var pingPaths = [
-        'api/devices?action=ping&code=S2BDCKLb1irqPU/iK5ijW1LvflNan8Drav9azvpC4ISa6A0oNeXp0A==',
+        '/api/devices?action=ping&code=S2BDCKLb1irqPU/iK5ijW1LvflNan8Drav9azvpC4ISa6A0oNeXp0A==',
         '/api/messages?action=ping&code=blfUL7SXmgc4gV1qQM9MFNIFymUh3AJEY7wDiMa3fxWyslmCEVvGpg==',
         '/api/message_feed?action=ping&code=nGYiKKC/z3FLR3yZb3sY71259NOtSVhqv/c178Ta5BJKsQTHe181AQ=='
     ]
