a963224d3916437f4c9f7a81ce5434d48505cd77
wtam <william.tam.2013@hotmail.com.hk>
2016-12-14 14:41:33 +0800

    mising the / from the ping path for device

--- dummy_timer/index.js
+++ dummy_timer/index.js
@@ -4,7 +4,7 @@ module.exports = function (context, myTimer) {
     context.log("Timer triggered at " + myTimer.next);
     //var https = require('https'); 
     var pingPaths = [
-        'api/devices?action=ping&code=y00bsO30Ri/1AZu1Otk6pew0EzqcdxrRQb/ArgI3j5VfrNpTzrhROA==',
+        '/api/devices?action=ping&code=y00bsO30Ri/1AZu1Otk6pew0EzqcdxrRQb/ArgI3j5VfrNpTzrhROA==',
         '/api/messages?action=ping&code=y00bsO30Ri/1AZu1Otk6pew0EzqcdxrRQb/ArgI3j5VfrNpTzrhROA==',
         '/api/message_feed?action=ping&code=y00bsO30Ri/1AZu1Otk6pew0EzqcdxrRQb/ArgI3j5VfrNpTzrhROA=='
     ]
